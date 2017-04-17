6798c908fac541f0311dc67d1816fd65a59a68f4
Kanasucre <rroy.romain@gmail.com>
2017-04-17 18:58:23 +0200

    Amélioration de la saisie

--- util.c
+++ util.c
@@ -14,28 +14,3 @@ char *saisir() {
     }
     return chaine;
 }
-
-/* Une autre fonction de saisie dynamique
-
-char *saisir() {
-    size_t taillbuff = 10;
-    char *buffer = malloc(taillbuff);
-    if (buffer == NULL) return NULL;
-    char *p;
-    for (p = buffer; *p = getchar() != '\n' && *p != EOF; p++)
-        if (p-buffer == taillbuff-1) {
-            p = realloc(buffer, taillbuff *= 2);
-            if (p == NULL) {
-                free(buffer);
-                return NULL;
-            } else buffer = p;
-            p += taillbuff/2-1;
-        }
-    *p = 0;
-    p = realloc(buffer,p-buffer+1);
-    if (p == NULL) {
-        free(buffer);
-        return NULL;
-    } else return p;
-}
-*/
