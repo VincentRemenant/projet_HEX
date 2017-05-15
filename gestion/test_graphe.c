2009c03b9601e7ccccf0f82f2752ff9c70c8e87d
Vincent Remenant <vincent.remenant@outlook.fr>
2017-05-15 16:56:08 +0200

    v0.2 Graphe terminé (seulement fonction ajout pour l'instant)

--- gestion/test_graphe.c
+++ gestion/test_graphe.c
@@ -3,6 +3,18 @@
 #include "graphe.h"
 
 int main(){
-  printf("Début des tests..\n");
-  printf("Tests terminés.\n");
+    graphe_t graphe = creer_graph(5);
+
+    ajouterSommet(&graphe, 0, 1);
+    ajouterSommet(&graphe, 0, 4);
+    ajouterSommet(&graphe, 1, 2);
+    ajouterSommet(&graphe, 1, 3);
+    ajouterSommet(&graphe, 1, 4);
+    ajouterSommet(&graphe, 2, 3);
+    ajouterSommet(&graphe, 3, 4);
+    
+    afficherGraphe(graphe);
+    detruire_graphe(&graphe);
+
+
 }
