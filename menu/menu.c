d6788622c022c894a1a80fbae338327799175e62
Kanasucre <rroy.romain@gmail.com>
2017-05-17 18:54:25 +0200

    Ajout des paramètres

--- menu/menu.c
+++ menu/menu.c
@@ -14,25 +14,25 @@ void menu(void) {
     do {
         printf(">>> ");
         choix = saisir();
-        if (strcmp(choix,"new") == 0) nouvellePartie();
-        else if (strcmp(choix,"save") == 0) /* sauvegarder(graphe_t plateau); */;
-        else if (strcmp(choix,"load") == 0) charger();
-        else if (strcmp(choix,"how") == 0) afficherRegles();
-        else if (strcmp(choix,"set") == 0) parametres(&taille, &difficulte);
-        else if (strcmp(choix,"quit") == 0)
+        if (strcmp(choix,"nouveau") == 0) nouvellePartie();
+        else if (strcmp(choix,"sauver") == 0) /* sauvegarder(graphe_t plateau); */;
+        else if (strcmp(choix,"charger") == 0) charger();
+        else if (strcmp(choix,"regles") == 0) afficherRegles();
+        else if (strcmp(choix,"options") == 0) parametres(&taille, &difficulte);
+        else if (strcmp(choix,"quitter") == 0)
             printf("Ce fut un plaisir de jouer avec vous, à bientôt !\n");
-        else if (strcmp(choix,"help") == 0) {
-            printf("new  : Commence une nouvelle partie\n");
-            printf("save : Enregistre une partie en cours\n");
-            printf("load : Reprend une partie enregistrée\n");
-            printf("how  : Affiche les règles du jeu de hex\n");
-            printf("set  : Permet d'accèder au paramètres\n");
-            printf("quit : Quitte le jeu\n");
+        else if (strcmp(choix,"aide") == 0) {
+            printf("nouveau : Commence une nouvelle partie\n");
+            printf("sauver  : Enregistre une partie en cours\n");
+            printf("charger : Reprend une partie enregistrée\n");
+            printf("regles  : Affiche les règles du jeu de Hex\n");
+            printf("options : Permet d'accèder au paramètres\n");
+            printf("quitter : Quitte le jeu\n");
         } else if (strcmp(choix,"") == 0)
             /* rien */;
         else
-            printf("Cette commande n'est pas reconnue : faites \"help\" pour obtenir la liste des commandes\n");
-    } while (strcmp(choix,"quit") != 0);
+            printf("Cette commande n'est pas reconnue : faites \"aide\" pour obtenir la liste des commandes\n");
+    } while (strcmp(choix,"quitter") != 0);
     free(choix);
 }
 
@@ -55,5 +55,26 @@ void afficherRegles(void) {
 }
 
 void parametres(int *taille, int *difficulte) {
-    // @Romain
+    char *choix;
+    do {
+        printf("Saisissez la taille de grille désirée : ");
+        choix = saisir();
+        *taille = atoi(choix);
+    } while (*taille < 5 || *taille > 50);
+    printf("La taille est désormais de %d.\n",*taille);
+    do {
+        printf("Choix de la difficulté : \n");
+        printf("\"joueur\" pour jouer contre un ami ou \"facile\" pour une IA facile.\n");
+        printf("Saisissez la difficulté désirée : ");
+        choix = saisir();
+        if (strcmp(choix,"joueur") == 0) {
+            *difficulte = 0;
+            printf("Le jeu est maintenant réglé en joueur contre joueur.\n");
+        }
+        else if (strcmp(choix,"facile") == 0) {
+            *difficulte = 1;
+            printf("Le jeu est maintenant réglé contre une IA facile.\n");
+        }
+        else printf("Choix non valide.\n");
+    } while (strcmp(choix,"joueur") != 0 && strcmp(choix,"facile") != 0);
 }
