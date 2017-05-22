30d18daa8e46a8dbf4f64e5c0f51fcbf3f31fa8c
Kanasucre <rroy.romain@gmail.com>
2017-05-22 15:53:07 +0200

    Ajout des en-têtes

--- util/util.h
+++ util/util.h
@@ -1,3 +1,9 @@
+/*************************************************************
+* util.h - Fonctions de bases utiles pour le jeu de hex      *
+* Dépendance :                                     *
+* Dernière modification : 22/05/2017                         *
+* Auteur : Romain Roy                                        *
+**************************************************************/
 
 char *saisir();
 
